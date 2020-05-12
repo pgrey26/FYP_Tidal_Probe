46bc0ca2ca0edcacf0f04ee15085597c2976a487
pgrey26 <57755695+pgrey26@users.noreply.github.com>
2020-05-12 09:13:51 +0100

    Define data size

--- 5HP_LowLatencyLogger/Logger.h
+++ 5HP_LowLatencyLogger/Logger.h
@@ -1,9 +1,29 @@
-// Avoid IDE problems by defining struct in septate .h file.
+// Avoid IDE problems by defining struct in separate .h file.
 // Pad record so size is a power of two for best write performance.
-#ifndef ExFatLogger_h
-#define ExFatLogger_h
-const size_t ADC_COUNT = 4;
+// Record size is 64 bytes.
+#ifndef Logger_h
+#define Logger_h
 struct data_t {
-  uint16_t adc[ADC_COUNT];
+  unsigned long time;
+  int16_t day;
+  int16_t month;
+  int32_t year;
+  int16_t hour;
+  int16_t minute;
+  int16_t second;
+  int16_t Pin1;
+  int16_t Pin2;
+  int16_t Pin3;
+  int16_t Pin4;
+  int16_t Pin5;
+  float ax;
+  float ay;
+  float az;
+  float gx;
+  float gy;
+  float gz;
+  float mx;
+  float my;
+  float mz;
 };
-#endif  // ExFatLogger_h
+#endif  // Logger_h
